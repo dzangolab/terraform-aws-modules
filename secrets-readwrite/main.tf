0039c238be1c9b9759e4ed1ad79a43ee82396b11
semanta <semanta@united-asian.com>
2019-08-07 11:25:10 +0545

    added module to create aws secrets read write policy

---
+++ secrets-readwrite/main.tf
@@ -0,0 +1,49 @@
+resource "aws_iam_policy" "readwrite_secret_policy" {
+  name   = var.policy_name
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "secretsmanager:GetResourcePolicy",
+                "secretsmanager:GetSecretValue",
+                "secretsmanager:DescribeSecret",
+                "secretsmanager:RestoreSecret",
+                "secretsmanager:PutSecretValue",
+                "secretsmanager:UpdateSecretVersionStage",
+                "secretsmanager:ListSecretVersionIds",
+                "secretsmanager:UpdateSecret"
+            ],
+            "Resource": [
+              "${join(",\n", var.secret_arns)}"
+            ]
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": "secretsmanager:GetRandomPassword",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_group_policy_attachment" "readwrite_secret_policy_group_attachment" {
+  count = length(var.groups)
+
+  group = element(var.groups, count.index)
+
+  policy_arn = aws_iam_policy.readwrite_secret_policy.arn
+}
+
+resource "aws_iam_user_policy_attachment" "readwrite_secret_policy_attachment" {
+  count = length(var.users)
+
+  user = element(var.users, count.index)
+
+  policy_arn = aws_iam_policy.readwrite_secret_policy.arn
+}
---
+++ secrets-readwrite/vars.tf
@@ -0,0 +1,22 @@
+variable "groups" {
+  default     = []
+  description = "Groups to attach policy to"
+  type        = list(string)
+}
+
+variable "policy_name" {
+  description = "Name of policy to be seen on console"
+  type        = string
+}
+
+variable "secret_arns" {
+  default     = []
+  description = "ARNs of secrets to add to the policy"
+  type        = list(string)
+}
+
+variable "users" {
+  default     = []
+  description = "Users to attach policy to"
+  type        = list(string)
+}
