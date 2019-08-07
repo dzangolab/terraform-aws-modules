06b30ab7319e1d1c89003beb9ed527f27764c651
semanta <semanta@united-asian.com>
2019-08-07 16:09:13 +0545

    updated test.tf

--- test.tf
+++ test.tf
@@ -1,11 +1,33 @@
+# test file to run `terraform validate` command
 provider "aws" {
   profile = "dzangolab"
   region  = "ap-southeast-1"
   version = "~> 2.16"
 }
 
-module "test_policy" {
+module "test_aws_docker_swarm" {
+  source = "./aws-docker-swarm"
+
+  eip_address = ""
+  eip_allocation_id = ""
+  key_pair_name = ""
+  swarm_name = ""
+}
+
+module "test_s3_bucket" {
+  source = "./s3-bucket"
+
+  policy_name = "test-s3-bucket"
+}
+
+module "test_secrets_readonly" {
+  source = "./secrets-readonly"
+  
+  policy_name = "test-secrets-readonly"
+}
+
+module "test_secrets_readwrite" {
   source = "./secrets-readwrite"
 
-  policy_name = "test"
+  policy_name = "test-secrets-readwrite"
 }
