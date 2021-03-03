1b1ca5964cd9eacd3ee409ecfc91b0176b9265f0
Olivier Pichon <op@dzangolab.com>
2021-03-03 11:54:12 +0700

    feat (ec2): allow custom root block device

--- ec2-docker-compose/main.tf
+++ ec2-docker-compose/main.tf
@@ -26,6 +26,18 @@ resource "aws_instance" "this" {
   })
   vpc_security_group_ids = var.vpc_security_group_ids
 
+  dynamic "root_block_device" {
+    for_each = var.root_block_device
+    content {
+      delete_on_termination = lookup(root_block_device.value, "delete_on_termination", null)
+      encrypted             = lookup(root_block_device.value, "encrypted", null)
+      iops                  = lookup(root_block_device.value, "iops", null)
+      kms_key_id            = lookup(root_block_device.value, "kms_key_id", null)
+      volume_size           = lookup(root_block_device.value, "volume_size", null)
+      volume_type           = lookup(root_block_device.value, "volume_type", null)
+    }
+  }
+
   tags = merge({ "Name" : var.name }, var.tags)
 }
 
